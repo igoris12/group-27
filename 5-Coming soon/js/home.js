2a16c331a68552143919138d0287fa4093e90518
igoris <igoris.x@inbox.lt>
2021-05-17 15:30:42 +0300

    socials finisch stile and layout

--- 5-Coming soon/js/home.js
+++ 5-Coming soon/js/home.js
@@ -1,3 +1,6 @@
 import { renderClock } from './components/clock/clock.js';
+import { socials } from './components/socials/socials.js';
+import { socialData } from './data/socialsData.js';
 
 renderClock('#hero_clock', '04-19 10:00:00');
+socials('#socials_block', socialData);
