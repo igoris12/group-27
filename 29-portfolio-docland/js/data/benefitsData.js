abad19c91980b1d3a185134de965042fbdb92d67
Igoris Ivanovas <igoris.x@inbox.lt>
2021-06-24 12:43:39 +0300

    benefits data

---
+++ 29-portfolio-docland/js/data/benefitsData.js
@@ -0,0 +1,54 @@
+const BenefitsData = {
+    imgPath: "/img/course - benefit - img /",
+    itemsInScrean: [
+        {
+            minWidth: 0,
+            itemsNumber: 1
+        },
+        {
+            minWidth: 750,
+            itemsNumber: 2
+        },
+        {
+            minWidth: 990,
+            itemsNumber: 3
+        }],
+    list: [
+        {
+            src: 'mentoring.png',
+            alt: 'Icon',
+            title: '1-1 Mentoring',
+            color: '#e9e6ff',
+
+        },
+        {
+            src: '100online.png',
+            alt: 'Icon',
+            title: '100% online',
+            color: '#6bf1d6',
+
+        },
+        {
+            src: 'flexibleDeadline.png',
+            alt: 'Icon',
+            title: 'Flexible Deadline',
+            color: '#ae88e6',
+
+        },
+        {
+            src: 'projectReviews.png',
+            alt: 'Icon',
+            title: 'Project Reviews',
+            color: '#fff6db',
+
+        },
+        {
+            src: 'mentoring.png',
+            alt: 'Icon',
+            title: '1-1 Mentoring',
+            color: '#e9e6ff',
+        },
+    ]
+}
+
+export { BenefitsData };
