72bdb754b9d3612bac9011ef0d288256c0f91c89
Igoris Ivanovas <igoris.x@inbox.lt>
2021-06-29 11:25:44 +0300

    reviews data

---
+++ 29-portfolio-docland/js/data/ReviewsData.js
@@ -0,0 +1,28 @@
+const ReviewsData = {
+    imgPath: './img/student-reviews-img/',
+    itemsInScrean: [
+        {
+            minWidth: 0,
+            itemsNumber: 1
+        },
+        {
+            minWidth: 770,
+            itemsNumber: 2
+        },
+    ],
+    list: [
+        {
+            img: 'sarahTaylor.png',
+            name: 'Sarah Taylor',
+            coment: 'The course has been really helpful. I have been able to understand the fundamentals ofUI Design. The importance of wireframing, prototyping, colours and how to study other site wireframing and to implement them into my UI/UX Creative process.'
+        },
+        {
+            img: 'dorjiTaylor.png',
+            name: 'Dorji Taylor',
+            coment: 'The course has been really helpful. I have been able to understand the fundamentals ofUI Design. The importance of wireframing, prototyping, colours and how to study other site wireframing and to implement them into my UI/UX Creative process.'
+        },
+    ]
+
+}
+
+export { ReviewsData }
