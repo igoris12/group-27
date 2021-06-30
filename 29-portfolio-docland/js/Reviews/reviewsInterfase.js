069703040021ef730b792e19d7ed9cb79c1d9ec0
Igoris Ivanovas <igoris.x@inbox.lt>
2021-06-30 11:38:27 +0300

    reviews carusel style end

--- 29-portfolio-docland/js/Reviews/reviewsInterfase.js
+++ 29-portfolio-docland/js/Reviews/reviewsInterfase.js
@@ -6,14 +6,18 @@ class InterfaceReviews {
 
     learnHTML() {
         const imgPath = this.data.imgPath + this.data.img
-        return `<div class="first">
-                <img class="taylor" src="${imgPath}" alt="Person">
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star-half-o checked"></span>
-                <h5>${this.data.name}</h5>
+        return `<div class="item">
+                    <div class="start">
+                        <img class="img" src="${imgPath}" alt="Person">
+                        <div class="stars">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star-half-o checked"></span>
+                            <h5>${this.data.name}</h5>
+                         </div>
+                </div>
                 <p class="smParagraph">${this.data.coment}</p>
             </div>`
     }
